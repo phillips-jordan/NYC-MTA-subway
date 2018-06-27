5729d7d1d89b0127f1132d53fe5c6fbce9a6e80b
phillips-jordan <jordan.phillips.nb@gmail.com>
2018-06-27 15:16:47 -0400

    starting frontend map

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,44 @@
 const express = require('express')
 const gtfs = require('gtfs');
 const mongoose = require('mongoose');
-const config = require('./config.json')
+const config = require("./config.json");
+const MongoClient = require("mongodb").MongoClient;
+const url = config.mongoUrl;
 const app = express();
 
-mongoose.connect(config.mongoUrl);
 
-gtfs
-  .import(config)
-  .then(() => {
-    console.log("Import Successful");
-    return mongoose.connection.close();
-  })
-  .catch(err => {
-    console.error(err);
-  });
+//used mongoose to import the GTFS data
 
-  app.listen(3000, ()=>{
+// mongoose.connect(config.mongoUrl);
+
+// gtfs
+// .import(config)
+// .then(() => {
+//     console.log("Import Successful");
+//     return mongoose.connection.close();
+// })
+// .catch(err => {
+//     console.error(err);
+// });
+
+
+app.get('/getAllStops', (req, res)=>{
+MongoClient.connect(
+  url,
+  (err, db) => {
+    if (err) throw err;
+    let dbo = db.db("nyc-mta-gtfs");
+    dbo.collection("stops").find({}).toArray((err, ret)=>{
+        if (err) throw err;
+        console.log(ret)
+        res.send(ret)
+        db.close();
+    })
+  }
+)}
+)
+
+
+app.listen(3000, ()=>{
       console.log('listening on 3000')
   })
